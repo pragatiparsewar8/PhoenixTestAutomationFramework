cce108fe01e3d814c8a903f3781318cbeb911908
Pragati P <pragatiparsewar@gmail.com>
2026-03-11 09:17:07 +0530

    Practised working with open csv library

---
+++ src/test/java/com/demo/csv/ReadCSVFile.java
@@ -0,0 +1,36 @@
+package com.demo.csv;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+public class ReadCSVFile {
+
+	public static void main(String[] args) throws IOException, CsvException {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/loginCreds.csv");
+		
+		InputStreamReader isr = new InputStreamReader(input);
+		CSVReader csvReader = new CSVReader(isr);
+		
+		List<String[]> dataList = csvReader.readAll();
+		
+		for(String[] dataArray:dataList) {
+			for(String data :dataArray) {
+				System.out.print(data+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
