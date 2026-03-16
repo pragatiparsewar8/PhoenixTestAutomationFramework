0dc69c02ba1654adda80d14a937365add483caae
Pragati P <pragatiparsewar@gmail.com>
2026-03-16 22:18:15 +0530

    Fixed the issue of creating random problem ids for the create job api
    Updated the generatefakeproblemlist

--- src/test/java/com/api/utils/FakerDataGenerator.java
+++ src/test/java/com/api/utils/FakerDataGenerator.java
@@ -25,6 +25,8 @@ public class FakerDataGenerator {
 	private static final int mst_oem_id = 1;
 	private static final int product_id = 1;
 	private static final int model_id = 1;
+	private static final int[] validProblemsId = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, 17, 19, 20, 22, 24,
+			26, 27, 28, 29 };
 
 	private FakerDataGenerator() {
 
@@ -36,34 +38,43 @@ public class FakerDataGenerator {
 		CustomerAddress customerAddress = generateFakeCustomerAddressData();
 		CustomerProduct product = generateFakeCustomerProduct();
 		List<Problems> problemList = generateFakeProblemsList();
-		CreateJobPayload payload = new CreateJobPayload(mst_service_location_id, mst_platform_id, mst_warrenty_status_id, mst_oem_id, customer, customerAddress, product, problemList);
+		CreateJobPayload payload = new CreateJobPayload(mst_service_location_id, mst_platform_id,
+				mst_warrenty_status_id, mst_oem_id, customer, customerAddress, product, problemList);
 		return payload;
 	}
-	
+
 	public static Iterator<CreateJobPayload> generateFakeCreateJobData(int count) {
-		
+
 		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
-		for(int i =1;i<= count ;i++) {
-		Customer customer = generateFakeCustomerData();
-		CustomerAddress customerAddress = generateFakeCustomerAddressData();
-		CustomerProduct product = generateFakeCustomerProduct();
-		List<Problems> problemList = generateFakeProblemsList();
-		CreateJobPayload payload = new CreateJobPayload(mst_service_location_id, mst_platform_id, mst_warrenty_status_id, mst_oem_id, customer, customerAddress, product, problemList);
-		payloadList.add(payload);
+		for (int i = 1; i <= count; i++) {
+			Customer customer = generateFakeCustomerData();
+			CustomerAddress customerAddress = generateFakeCustomerAddressData();
+			CustomerProduct product = generateFakeCustomerProduct();
+			List<Problems> problemList = generateFakeProblemsList();
+			CreateJobPayload payload = new CreateJobPayload(mst_service_location_id, mst_platform_id,
+					mst_warrenty_status_id, mst_oem_id, customer, customerAddress, product, problemList);
+			payloadList.add(payload);
 		}
 		return payloadList.iterator();
 	}
 
 	private static List<Problems> generateFakeProblemsList() {
 		// TODO Auto-generated method stub
-		String remark = faker.lorem().sentence(3);
 
-		int problemId = RANDOM.nextInt(26) + 1;
-
-		Problems problem = new Problems(problemId, remark);
-
+		int count = RANDOM.nextInt(3) + 1;
+		String remark;
+		int randomIndex;
+		Problems problem;
 		List<Problems> problemList = new ArrayList<Problems>();
-		problemList.add(problem);
+		for (int i = 0; i < count; i++) {
+			remark = faker.lorem().sentence(3);
+
+			randomIndex = RANDOM.nextInt(validProblemsId.length);
+
+			problem = new Problems(validProblemsId[randomIndex], remark);
+
+			problemList.add(problem);
+		}
 		return problemList;
 	}
 
@@ -75,7 +86,7 @@ public class FakerDataGenerator {
 
 		CustomerProduct product = new CustomerProduct(dop, imeiSerialNumber, imeiSerialNumber, imeiSerialNumber, popUrl,
 				product_id, model_id);
-		
+
 		return product;
 	}
 
