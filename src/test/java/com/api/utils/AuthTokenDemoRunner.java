28eb10f3ceeaa15d871ca9135a9fbfdfec1a9d5c
Pragati P <pragatiparsewar@gmail.com>
2026-03-29 13:01:17 +0530

    Improved token caching

---
+++ src/test/java/com/api/utils/AuthTokenDemoRunner.java
@@ -0,0 +1,12 @@
+package com.api.utils;
+
+import com.api.constants.Role;
+
+public class AuthTokenDemoRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AuthTokenProvider.getToken(Role.FD);
+	}
+
+}
